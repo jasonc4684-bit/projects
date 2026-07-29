afb31b8d6ae143af314e6456a7ff592de06bf894
jasonc4684-bit <jasonc4684@gmail.com>
2026-07-29 19:44:08 +0000

    .

--- candy_cane.py
+++ candy_cane.py
@@ -4,7 +4,6 @@ candy_height = [int(i) for i in input.split()]
 
 for i in range(M):
     total_candy_eaten = 0
-
     for j in range(N):
         eaten += min(candy_height[i], cows_height[j]) - total_candy_eaten
         if eaten <= 0:
@@ -14,5 +13,5 @@ for i in range(M):
         if total_candy_eaten == candy_height[i]:
             break
 
-for i in range(j):
+for i in range(N):
     print(cows_height[i])
